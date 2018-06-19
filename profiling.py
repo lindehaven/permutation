e0a5d0949e41c8919962a19d7ae28f3aaf0cb171
Lars Lindehaven <lindehaven@users.noreply.github.com>
2018-06-19 16:17:50 +0200

    Interactive profiling
    Type 1-n symbols. Note that n>8 takes quite some time to calculate.

--- profiling.py
+++ profiling.py
@@ -1,6 +1,7 @@
 import cProfile
 import pstats
 import permutation_strings
-cProfile.run('permutation_strings.calculate("DA")', 'restats')
+arg = 'permutation_strings.calculate("'+input("Type symbols: ")+'")'
+cProfile.run(arg, 'restats')
 p = pstats.Stats('restats')
 p.sort_stats('cumulative').print_stats(20)
